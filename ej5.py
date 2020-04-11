bb84542d39cba24a45b821703d5e7509b99682f2
gomezlucia <62036346+gomezlucia@users.noreply.github.com>
2020-04-11 11:50:26 -0300

    Add files via upload

---
+++ ej5.py
@@ -0,0 +1,40 @@
+numeros=[]
+j=-1
+print('menu de opciones para la lista de numeros a ingresar')
+print('1: ingresar numeros')
+print('2: ordenar numeros')
+print('3: calcular el maximo')
+print('4: calcular el minimo')
+print('5: calcular promedio')
+print('0: terminar')
+input(n)
+while (n!=0):
+    if (n==1):
+        print('ingrese numero')
+        input(r)
+        j=j+1
+        numeros[j]=r
+    else:
+        if (n==2):
+#como pregunto si la lista esta vacia?
+            numeros.sort()
+        else:
+            if (n==3):
+#no entendi como funciona la funcion de maximo
+            else:
+                if (n==4):
+                    #la funcion minimo tampoco entendi
+                else:
+                    if (n==5):
+                        t=sum(numeros[])
+                        r= # no habia anotado cual es la funcion que indica la cantidad de elementos de una lista
+                        prom= t/r
+                        print(prom)
+    print('menu de opciones para la lista de numeros a ingresar')
+    print('1: ingresar numeros')
+    print('2: ordenar numeros')
+    print('3: calcular el maximo')
+    print('4: calcular el minimo')
+    print('5: calcular promedio')
+    print('0: terminar')
+    input(n)
